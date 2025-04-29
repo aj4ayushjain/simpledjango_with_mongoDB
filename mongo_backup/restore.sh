61468772535b1f15e0c23d303a27a6e6009ceb66
Ayush Jain <aj4ayushjain@gmail.com>
2025-04-29 16:35:33 +0530

    Cleanup old mongo backup files
    Signed-off-by: Ayush Jain <aj4ayushjain@gmail.com>

--- mongo_backup/restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# start mongod
-nohup mongod --bind_ip_all
-# Restore the database from the .agz file
-echo "Restoring MongoDB from transactions.agz..."
-mongorestore --gzip --archive=/data/transactions.agz
-
